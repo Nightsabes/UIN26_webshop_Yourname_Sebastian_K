dff4a018a56be7f822d15caece6ef133755b53ce
Nightsabes <39102045+Nightsabes@users.noreply.github.com>
2026-01-16 13:13:13 +0100

    help

--- LEGOdudes/shopscript.js
+++ LEGOdudes/shopscript.js
@@ -19,10 +19,45 @@ function fetchProducts() {
 
  fetchProducts()
 
+ //Generer handlevogn :P
+
+ function showCart(){
+    //Unike produkter and stuffus
+    let uniqueItems = new Set(cart)
+    let uniqueArray = [...uniqueItems]
+    //oversikt over antall per produkt XDDDDDDDDDDDDDDDD
+    let cartItems = []
+    uniqueArray.map(item => {
+        cartItems.push({prodid: item, quantity: cart.filter(i => i === item).length})
+    })
+    //console.log(cartItems)
+    //Gå gjennom cart items for å lage HTML til handelkurven og regne ut total priprprrssssssjijsfis:
+    let cartHTML = ""
+    let totalPrice = 0
+    
+    
+    cartItems.map(ci => {
+        //hente produktinformasjon
+        let product = products.find(i => i.prodid === ci.prodid)
+        //Skrive ut HTML
+        cartHTML += `<tr>
+                    <td class="title">${product.title}</td>
+                    <td class="pris">${product.price}</td>
+                    <td class="quantity">${ci.quantity}</td>
+                    <td class="delete"><button>X</button></td>
+                </tr>`
+    })
+    document.getElementById("cart-items").innerHTML = cartHTML
+ }
+
+ 
  //Leg til i handle avang
 function addToCart(prodid) {
     console.log("Legg til produkt med id: " + prodid)
     cart.push(prodid)
     console.log(cart)
     document.getElementById("cart-quantity").innerHTML = cart.length
+
+    //Oppdater handlevogn visning::::::::::::::
+    showCart()
 }
